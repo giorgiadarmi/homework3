17d1c7e3a596dd90d7d6b90281349dfe9d24e250
giorgia.darmiento <giorgia.darmiento@PCGIORGIA>
2022-10-17 20:57:10 +0200

    Aggiungere i file di progetto.

---
+++ homework3/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(36, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Wireshark"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(36, 70)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(731, 351)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ homework3/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Path As String = "C:\Users\giorgia.darmiento\source\repos\homework3\wireshark.csv"
+
+        Dim ListOfUnits As New List(Of Packet)
+        Me.RichTextBox1.AppendText("Distribution of data from wireshark:" & Environment.NewLine & Environment.NewLine)
+
+        Using R As New TextFieldParser(Path)
+
+            R.Delimiters = New String() {","}
+
+            Dim NamesOfVariables As String = R.ReadLine
+
+            Do While Not R.EndOfData
+
+                Dim Values() As String = R.ReadFields()
+
+                Dim Packet As New Packet
+
+                'fille the fields with actual data
+
+                Packet.No = Values(0)
+                Packet.Time = Values(1)
+                Packet.Source = Values(2)
+                Packet.Destination = Values(3)
+                Packet.Protocol = Values(4)
+                Packet.Length = Values(5)
+                Me.RichTextBox1.AppendText(" No: " & Packet.No.ToString().PadRight(15) & " Time: " & Packet.Time.ToString().PadRight(15) &
+                                           " Source: " & Packet.Source.ToString().PadRight(80) & " Destination: " & Packet.Destination.ToString().PadRight(80) &
+                                           " Protocol: " & Packet.Protocol.ToString().PadRight(15) & " Length: " & Packet.Length.ToString().PadRight(15) & Environment.NewLine & Environment.NewLine)
+                Dim ListOfLength As New List(Of Double)
+                ListOfLength.Add(CDbl(Packet.Length))
+
+                Dim Avarage_Online As Double = Me.ComputationOfArithmeticMean_OnlineAlgo(ListOfLength)
+                Me.RichTextBox1.AppendText("Arithmetic mean of length (online formula):  " & Avarage_Online & Environment.NewLine)
+                ListOfUnits.Add(Packet)
+
+
+
+            Loop
+
+        End Using
+
+
+
+
+    End Sub
+
+    Function ComputationOfArithmeticMean_OnlineAlgo(ListOfLength As List(Of Double)) As Double
+        Dim Avarage_Online As Double = 0
+        Dim CurrentIndex As Integer = 0
+
+        For Each v As Double In ListOfLength
+            CurrentIndex += 1
+            Avarage_Online += (v - Avarage_Online) / CurrentIndex
+        Next
+        Return Avarage_Online
+    End Function
+
+End Class
+
+Public Class Packet
+    Public No As Integer
+    Public Time As String
+    Public Source As String
+    Public Destination As String
+    Public Protocol As String
+    Public Length As String
+End Class
